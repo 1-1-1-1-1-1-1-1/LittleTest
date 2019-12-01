baf32e88a627e22dbda8904c8082ea0030b5f3ef
1-1-1-1-1-1-1-1 <57982793+1-1-1-1-1-1-1-1@users.noreply.github.com>
2019-12-01 12:47:41 +0200

    bot.py was corrected

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ bot = telebot.TeleBot('707207156:AAErS3Wvvf0tjaU8nwhZJHu-UcFHipdM-b8')
 @bot.message_handler (commands = ['start'])
 def start_message(message):
     c = message.chat
-    bot.send_message (n, '''Текущее имя этого чата: ' + c.first_name + ' ' + c.last_name
-    Тип чата: ' + c.type
-    id чата: c.id)
+    bot.send_message (c.id, '''Текущее имя этого чата: {} {}
+    Тип чата: {}
+    id чата: {}'''.format(c.first_name, c.last_name, c.type, c.id))
 
 bot.polling (none_stop = True)
